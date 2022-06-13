6c3d9c3d52abd74db0a8cebaec1c7561e70549eb
HasiburQOR <hasibur.qor@gmail.com>
2022-06-13 14:41:15 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,14 @@
+function sum(...theArgs) {
+    let total = 0;
+    for (const arg of theArgs) {
+      total += arg;
+    }
+    return total;
+  }
+  
+  console.log(sum(1, 2, 3));
+
+  
+  console.log(sum(1, 2, 3, 4));
+
+  
